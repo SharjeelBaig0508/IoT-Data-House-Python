02689ff799d45c4dbc6d039f3a34c5e674f4f0d3
SharjeelBaig0508 <baig.sharjeel@yahoo.com>
2022-08-28 18:08:54 +0500

    CHANGE: implemented Environment Variables for Logging

--- app.py
+++ app.py
@@ -3,25 +3,24 @@ from flask import Flask
 from flask_cors import CORS
 from database_layer import initialize_database
 
+# ---------< Environment Variables >---------
+from os import environ
+import dotenv
+if dotenv.find_dotenv():
+    dotenv.load_dotenv()
+
 # ---------< Logging Implementation >---------
 import app_logger
 app_logger.logger_initializer(
     app_name = __name__, 
-    logger_name = 'logger', 
+    logger_name = environ.get('LOGGER_NAME'), 
     logging_format = '[%(asctime)s] [%(levelname)s] [%(remote_addr)s] requested [%(url)s %(method)s] [%(filename)s:%(lineno)d] in %(module)s" %(message)s with params: %(params)s and body: %(body)s',
-    logging_level = 'DEBUG'
+    logging_level = environ.get('LOGGER_LEVEL')
 )
 
 import global_variables
 rootLogger = global_variables.multiprocess_globals['logger']
 logger = rootLogger.getChild(__name__)
-
-# ---------< Environment Variables >---------
-from os import environ
-import dotenv
-if dotenv.find_dotenv():
-    logger.info("Loaded Environment")
-    dotenv.load_dotenv()
 
 # -----------< Flask App Initialization >----------- 
 app = Flask(__name__)
