c6ea4d264bc59db5db460701da27be77218c9f64
SharjeelBaig0508 <baig.sharjeel@yahoo.com>
2022-08-30 14:50:37 +0500

    CHANGE: added the field hiding implementation in logs

--- app_logger.py
+++ app_logger.py
@@ -3,6 +3,7 @@ from flask import has_request_context, request
 import logging
 import global_variables
 
+to_hide_fields_from_body = ['password']
 class RequestFormatter(logging.Formatter):
     def format(self, record):
         if has_request_context():
@@ -11,6 +12,12 @@ class RequestFormatter(logging.Formatter):
             record.remote_addr = request.remote_addr
             record.params = request.args.to_dict()
             record.body = request.get_json()
+            if record.body and type(record.body) is dict:
+                shortened_data = {k: "_______" for k in to_hide_fields_from_body if k in record.body and record.body[k]}
+                if shortened_data:
+                    to_be_shortened_data = {k: record.body[k] for k in record.body}
+                    to_be_shortened_data.update(shortened_data)
+                    record.body = to_be_shortened_data
         else:
             record.url = 'No Route'
             record.method = 'No Method'
