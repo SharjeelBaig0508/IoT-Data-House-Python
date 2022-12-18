843ed001c2d0c1d68d4382bc50c9899313d98b3a
SharjeelBaig0508 <baig.sharjeel@yahoo.com>
2022-12-18 19:27:57 +0500

    CHANGE: improved the blueprint implementation

--- views/__init__.py
+++ views/__init__.py
@@ -1,14 +1,33 @@
 from flask import Blueprint
-from views.v1.users import (
-    login as user_login,
-    signup as user_signup,
-    self_operations as user_on_self,
-)
+from views.v1 import users
+
+# All the methods used in backend APIs
+GET, POST, PUT, DELETE = 'GET', 'POST', 'PUT', 'DELETE'
 
 v1_views = Blueprint('v1_views', __name__, url_prefix='/api/v1')
 
-v1_views.add_url_rule('/user/login', view_func=user_login, methods=['POST'])
+# All routes available
+all_rules = {
+    'user': [
+        # ROUTE, VIEW, METHODS
+        [
+            '/user/login',
+            users.login,
+            [POST],
+        ],
+        [
+            '/user/signup',
+            users.signup,
+            [POST],
+        ],
+        [
+            '/user',
+            users.self_operations,
+            [GET, PUT, DELETE],
+        ],
+    ],
+}
 
-v1_views.add_url_rule('/user/signup', view_func=user_signup, methods=['POST'])
-
-v1_views.add_url_rule('/user', view_func=user_on_self, methods=['GET', 'PUT', 'DELETE'])
+for type in all_rules:
+    for rule in all_rules[type]:
+        v1_views.add_url_rule(rule[0], view_func=rule[1], methods=rule[2])
